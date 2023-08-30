5df38ccdae4cb562fbe7f1f5ee2949286e0fd4da
Pranau-R <pranau@mcci.com>
2023-08-30 17:00:19 +0530

    Initial commit

---
+++ build-with-cli.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+##############################################################################
+#
+# Module: build-with-cli.sh
+#
+# Function:
+#	This script sets variables and calls the common build.
+#
+# Usage:
+#	build-with-cli.sh --help
+#
+# Copyright and License:
+#	See accompanying LICENSE.md file
+#
+# Author:
+#	Terry Moore, MCCI	August 2023
+#
+##############################################################################
+
+set -e
+
+# get the path to this file as the default for the build path.
+PDIR=$(realpath "$(dirname "$0")")
+
+# export the root name of this script for use by build-common.
+PNAME=$(basename "$0")
+export PNAME
+
+# so the build directory shows up where this script lives, CD here.
+cd "$PDIR"
+
+# launch the common script and pass in the args.
+. "${PDIR}/extra/tools-build-with-cli/build-with-cli-lib.sh"
+
+function _setProject {
+    #---- project settings -----
+    readonly OPTOUTPUTNAME_DEFAULT=catena-4430-mfgtest-complete
+    readonly OPTKEYFILE_DEFAULT="$INVOKEDIR/keys/project.pem"
+    readonly OPTARDUINO_BOARD_DEFAULT=4610
+    readonly OPTREGION_DEFAULT=us915
+    readonly OPTNETWORK_DEFAULT=ttn
+    readonly OPTSUBBAND_DEFAULT=default
+    readonly OPTCLOCK_DEFAULT=16
+    readonly OPTXSERIAL_DEFAULT=hw
+    readonly OPTARDUINO_SOURCE_DEFAULT=sketches/catena4430-mfgtest-complete/catena4430-mfgtest-complete.ino
+}
+
+# result is version as either x.y.z-N or x.y.z (if pre-release tag is -0)
+_debug "Override _getversion"
+function _getversion {
+    sed -n \
+        -e 's/^constexpr.* kAppVersion .*makeVersion[ \t]*([ \t]*\([0-9]*\)[ \t]*,[ \t]*\([0-9]*\)[ \t]*,[ \t]*\([0-9]*\)[ \t]*,[ \t]*\([0-9]*\)[ \t]*).*/\1.\2.\3_pre\4/p' \
+        -e 's/^constexpr.* kAppVersion .*makeVersion[ \t]*([ \t]*\([0-9]*\)[ \t]*,[ \t]*\([0-9]*\)[ \t]*,[ \t]*\([0-9]*\)[ \t]*).*/\1.\2.\3/p' \
+            "$1" | \
+    sed -e 's/_pre0$//'
+}
+
+_doBuild "$@"
